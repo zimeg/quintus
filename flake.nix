1b50c03bf441a203e8ab0d51fd141734b4cdad34
@zimeg <zim@o526.net>
2025-05-26 19:48:55 -0700

    build: nest packages in an ignored result for kept setups together

--- flake.nix
+++ flake.nix
@@ -53,74 +53,76 @@
           '';
         };
       });
-      packages."aarch64-darwin".default = cicero."aarch64-darwin";
-      packages."aarch64-linux".default = cicero."aarch64-linux";
-      packages."x86_64-darwin".default = cicero."x86_64-darwin";
-      packages."x86_64-linux" = {
-        default = cicero."x86_64-linux";
-        tullius =
-          let
-            system = "x86_64-linux";
-            pkgs = import nixpkgs {
-              inherit system;
-            };
-            configurations = {
-              system.stateVersion = "24.05";
-              nix.registry = {
-                nixpkgs.flake = nixpkgs;
+      packages = {
+        "aarch64-darwin".default = cicero."aarch64-darwin";
+        "aarch64-linux".default = cicero."aarch64-linux";
+        "x86_64-darwin".default = cicero."x86_64-darwin";
+        "x86_64-linux" = {
+          default = cicero."x86_64-linux";
+          tullius =
+            let
+              system = "x86_64-linux";
+              pkgs = import nixpkgs {
+                inherit system;
               };
-              networking = {
-                hostName = "tullius";
-                firewall = {
-                  enable = true;
-                  allowedTCPPorts = [ 80 ];
-                  allowedUDPPorts = [ 123 ];
+              configurations = {
+                system.stateVersion = "24.05";
+                nix.registry = {
+                  nixpkgs.flake = nixpkgs;
                 };
-                networkmanager = {
-                  enable = true;
-                };
-              };
-              systemd.services.cicero = {
-                enable = true;
-                wantedBy = [ "multi-user.target" ];
-                after = [ "network.target" ];
-                script = ''
-                  ${cicero.${system}}/bin/cicero
-                '';
-                serviceConfig = {
-                  Restart = "always";
-                  Type = "simple";
-                };
-              };
-              time = {
-                timeZone = "Etc/UTC";
-              };
-              virtualisation = {
-                diskSize = 4 * 1024;
-              };
-            };
-            name = "cicero-${system}";
-            image = inputs.nixos-generators.nixosGenerate {
-              inherit pkgs;
-              format = "amazon";
-              modules = [
-                configurations
-                {
-                  amazonImage = {
-                    inherit name;
+                networking = {
+                  hostName = "tullius";
+                  firewall = {
+                    enable = true;
+                    allowedTCPPorts = [ 80 ];
+                    allowedUDPPorts = [ 123 ];
                   };
-                }
-              ];
+                  networkmanager = {
+                    enable = true;
+                  };
+                };
+                systemd.services.cicero = {
+                  enable = true;
+                  wantedBy = [ "multi-user.target" ];
+                  after = [ "network.target" ];
+                  script = ''
+                    ${cicero.${system}}/bin/cicero
+                  '';
+                  serviceConfig = {
+                    Restart = "always";
+                    Type = "simple";
+                  };
+                };
+                time = {
+                  timeZone = "Etc/UTC";
+                };
+                virtualisation = {
+                  diskSize = 4 * 1024;
+                };
+              };
+              name = "cicero-${system}";
+              image = inputs.nixos-generators.nixosGenerate {
+                inherit pkgs;
+                format = "amazon";
+                modules = [
+                  configurations
+                  {
+                    amazonImage = {
+                      inherit name;
+                    };
+                  }
+                ];
+              };
+              virtualization = "${image}/${name}.vhd";
+            in
+            {
+              inherit cicero;
+              tofu = pkgs.writeShellScriptBin "tofu" ''
+                export TF_VAR_image="${virtualization}"
+                ${pkgs.opentofu}/bin/tofu $@
+              '';
             };
-            virtualization = "${image}/${name}.vhd";
-          in
-          {
-            inherit cicero;
-            tofu = pkgs.writeShellScriptBin "tofu" ''
-              export TF_VAR_image="${virtualization}"
-              ${pkgs.opentofu}/bin/tofu $@
-            '';
-          };
+        };
       };
     };
 }
